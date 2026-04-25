f728d5b1b9a8c89425edc99324a07a5ca1ad9a7a
User_at3am <prajapatiaman18113@gmail.com>
2026-04-25 17:31:56 +0530

    Initial Commit

---
+++ SweepIn/SweepInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweepInApp.swift
+//  SweepIn
+//
+//  Created by apple on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SweepInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
